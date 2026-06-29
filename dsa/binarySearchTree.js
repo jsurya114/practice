f383283bcf881ee4cca33359f6766bd2df3eefd9
Jayasurya S <jayasuryas@Jayasuryas-MacBook-Air-2.local>
2026-06-29 22:10:15 +0530

    feat:dsa/singlylist

---
+++ dsa/binarySearchTree.js
@@ -0,0 +1,99 @@
+class Node{
+    constructor(value){
+        this.value=value
+        this.left=null
+        this.right=null
+    }
+}
+
+class BST{
+    constructor(){
+        this.root=null
+    }
+
+    insert(value){
+        const node = new Node(value)
+        if(this.root===null){
+            return this.root=node
+        }else{
+            this.inserNode(this.root,node)
+        }
+    }
+    inserNode(root,node){
+        if(root.value>node.value){
+            if(root.left===null){
+                root.left=node
+                return
+            }else{
+                this.inserNode(root.left,node)
+            }
+        }else{
+            if(root.right===null){
+               root.right=node
+               return
+            }else{
+                this.inserNode(root.right,node)
+            }
+        }
+    }
+
+     inorder(root){
+        if(root){
+            this.inorder(root.left)
+            console.log(root.value)
+            this.inorder(root.right)
+        }
+    }
+    postorder(root){
+        if(root){
+            this.postorder(root.left)
+            this.postorder(root.right)
+            console.log(root.value)
+        }
+    }
+    preorder(root){
+        if(root){
+            console.log(root.value)
+            this.preorder(root.left)
+            this.preorder(root.right)
+        }
+    }
+
+    min(root){
+        if(!root.left){
+            return root.value
+        }else{
+            return this.min(root.left)
+        }
+
+    }
+
+    delete(value){
+        this.root=this.deleteNode(this.root,value)
+    }
+
+    deleteNode(root,value){
+        if(!root) return null
+
+        if(root.value>value){
+            root.left=this.deleteNode(root.left,value)
+        }else if(root.value<value){
+            root.right=this.deleteNode(root.right,value)
+        }else{
+            if(!root.left&&!root.left) return null
+
+            if(!root.left) return root.right
+            if(!root.right) return root.left
+
+            root.value = this.min(root.right)
+            root.right = this.deleteNode(root.right,root.value)
+        }
+        return root
+    }
+
+    search(root,target){
+        
+    }
+
+    
+}
---
+++ dsa/singlylist.js
@@ -0,0 +1,101 @@
+class Node {
+    constructor(value){
+        this.value=value
+        this.next=null
+    }
+}
+
+class SingleList{
+    constructor(){
+        this.head=null
+        this.size=0
+    }
+    prepend(value){
+        const node = new Node(value)
+        node.next=this.head
+        this.head=node
+        this.size++
+    }
+    append(value){
+        const node = new Node(value)
+        
+        if(!this.head){
+            this.head=node
+            this.size++
+            return
+        }
+        let curr = this.head
+        while(curr.next){
+            curr=curr.next
+        }
+        curr.next=node
+        this.size++
+    }
+    addValue(index,value){
+        const node = new Node(value)
+        
+        if(index===0){
+            return this.prepend(value)
+        }
+        if(index===this.size) return this.append(value)
+        let curr = this.head
+        for(let i=0;i<index-1;i++){
+            curr=curr.next
+        }
+        node.next=curr.next
+        curr.next=node
+        this.size++
+        
+    }
+    
+    remove(index){
+        if(!this.head) return null
+        
+        if(index===0){
+            this.head=this.head.next
+            this.size--
+            return
+        }
+        
+        let curr = this.head
+        for(let i=0;i<index-1;i++){
+            curr=curr.next
+        }
+        curr.next=curr.next.next
+        this.size--
+    }
+    removeV(value){
+        if(!this.head) return null
+        
+        if(this.head.value===value){
+            this.head=this.head.next
+            this.size--
+            return
+        }
+        let curr = this.head
+        while(curr.next){
+            if(curr.next.value===value){
+                curr.next=curr.next.next
+            }
+            curr=curr.next
+            this.size--
+        }
+    }
+    print(){
+        let curr = this.head
+        while(curr){
+            console.log(curr.value)
+            curr=curr.next
+        }
+    }
+}
+
+
+const sl = new SingleList()
+let arr = [1,2,3,4,5,6]
+for(let i=0;i<arr.length;i++){
+    sl.append(arr[i])
+}
+sl.addValue(6,7)
+sl.removeV(2)
+sl.print()
