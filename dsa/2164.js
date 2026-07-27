abb0177cf47e25ad40c6e0921e2fd591c995bcbf
jsurya114 <jayasuryabrocamp@gmail.com>
2026-07-27 16:51:43 +0530

    leetcode 2164

---
+++ dsa/2164.js
@@ -0,0 +1,28 @@
+function sortevenodd(arr){
+    let even =[]
+    let odd=[]
+
+    for(let i=0;i<arr.length;i++){
+        if(i%2===0){
+            even.push(arr[i])
+        }else{
+            odd.push(arr[i])
+        }
+    }
+
+    even.sort((a,b)=>a-b)
+    odd.sort((a,b)=>b-a)
+
+    let e=0
+    let o=0
+    for(let i=0;i<arr.length;i++){
+        if(i%2===0){
+            arr[i]=even[e++]
+        }else{
+            arr[i]=even[o++]
+        }
+    }
+    return arr
+}
+let arr=[4,1,2,3]
+console.log(sortevenodd(arr))
